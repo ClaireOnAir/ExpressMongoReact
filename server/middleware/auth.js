dcc164f0600cdb4640360116282158cb9cacaa04
ClaireOnAir <andraia1994@gmail.com>
2024-07-26 13:45:34 +0200

    Endpoints, auth, login creados y terminado el proyecto GG WELL PLAYED

---
+++ server/middleware/auth.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  // Autorización por el header
+  const authHeader = req.get("Authorization");
+
+  if (!authHeader) {
+    const error = new Error("No autenticado, no hay JWT");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  // Obtener el token y verificarlo
+  const token = authHeader.split(" ")[1];
+  let revisarToken;
+  try {
+    revisarToken = jwt.verify(token, "LLAVESECRETA");
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+
+  // Si es un token válido pero hay algún error
+  if (!revisarToken) {
+    const error = new Error("No autenticado");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  next();
+};
